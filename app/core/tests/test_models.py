168bd516773fc0b5a68e6ae60a2336ec7215d78d
c1pher <c1pher@DESKTOP-EC1V0EH.localdomain>
2022-10-26 02:30:56 +0530

    Added endpoints for tags model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,6 +12,7 @@ def sample_user(email='test@gmail.com', password='test123'):
     """
     return get_user_model().objects.create_user(email, password)
 
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """
